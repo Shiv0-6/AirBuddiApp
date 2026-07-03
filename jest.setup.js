fc5f0cc05baec8f595e27f4010bc22e56f9ce463
shiv0-6 <csefw2306@glbitm.ac.in>
2026-07-03 07:02:09 -0400

    refactor jest setup for react-native-gesture-handler mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,11 @@
 import 'react-native-gesture-handler/jestSetup';
 
-jest.mock('react-native-gesture-handler', () => require('react-native-gesture-handler/jest/mock'));
+jest.mock('react-native-gesture-handler', () => {
+	const React = require('react');
+	const { View } = require('react-native');
+
+	return {
+		GestureHandlerRootView: ({children, ...props}) => React.createElement(View, props, children),
+	};
+});
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
